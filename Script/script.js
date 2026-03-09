de01b77bea596259d4e579e8208df954c3e9e041
arifkhanever <mdshafiqul0404@gmail.com>
2026-03-09 19:01:45 +0600

    Modal implementated

--- Script/script.js
+++ Script/script.js
@@ -48,6 +48,57 @@ function activeTabs(id) {
     }
 }
 
+async function loadModal(id){
+    const response = await fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`);
+    const data = await response.json();
+
+    const modalPop = document.getElementById('modalPop');
+
+    modalPop.innerHTML = `
+        <dialog id="my_modal" class="modal modal-bottom sm:modal-middle">
+            <div class="modal-box space-y-4">
+                <div class="heading">
+                    <h2 class="text-[24px] font-bold">${data.data.title}</h2>
+                    <div>
+                        <span class="priority px-5 py-1 text-[12px] ${data.data.status === "open"? "bg-green-100 text-green-500": "bg-purple-200 text-purple-500"} rounded-full">${data.data.status}</span><span class="py-1 text-[12px] text-[#64748B]"> • Opened by ${data.data.author} • ${new Date(data.data.createdAt).toLocaleDateString()}</span>
+
+                    </div>
+                </div>
+
+                <div class="labels flex items-center gap-2">
+                    <p class="bug  px-2  uppercase text-[12px] ${data.data.labels[0] === 'enhancement' ? 'bg-green-100 text-green-600' : data.data.labels[0] === 'documentation' ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600'} rounded-full">${data.data.labels[0]}</p>
+                    <p class="help px-2 uppercase text-[12px] ${data.data.labels[1] === 'enhancement' ? 'bg-green-100 text-green-600' : data.data.labels[1] === 'documentation' ? 'bg-green-100 text-green-600' : 'bg-red-100 text-red-600'} rounded-full">${data.data.labels[1]}</p>
+                </div>
+
+                <div class="des">
+                    <p class="cardDsc text-[16px] text-[#64748B] mt-[-7px]">${data.data.description}</p>
+                </div>
+
+                <div class="author bg-gray-100 p-5 rounded-lg grid grid-cols-2">
+                    <div class="name">
+                        <p class="text-gray-500 mb-1">Assignee:</p>
+                        <h6>${data.data.assignee === ""?"Unknown": data.data.assignee}</h6>
+                    </div>
+                    <div class="priority text-left">
+                        <p class="text-gray-500 mb-1">Priority:</p>
+                        <span class="priority px-5 py-1 uppercase text-[12px] ${data.data.priority ==='high'? "bg-red-100 text-red-500": data.data.priority ==='medium'? "bg-orange-100 text-orange-500": "bg-gray-200 text-gray-500"} rounded-full">${data.data.priority}</span>
+                    </div>
+                </div>
+
+                <form method="dialog" class="flex justify-end">
+                    <button class="btn btn-primary">Close</button>
+                </form>
+                </div>
+            </div>
+        </dialog>
+    `;
+
+
+    const modalBtn = document.getElementById('my_modal');
+    modalBtn.showModal();
+
+}
+
 function displayIssues(data) {
     const issueCards = document.getElementById('issueCards');
     issueCards.innerHTML = "";
@@ -60,7 +111,7 @@ function displayIssues(data) {
         const cardDiv = document.createElement('div');
 
         cardDiv.innerHTML = `
-            <div class="card bg-white border-t-4 h-full rounded-md border-${el.status == "open"? "green":"purple"}-400 shadow-sm">
+            <div onclick="loadModal(${el.id})" class="card bg-white border-t-4 h-full rounded-md border-${el.status == "open"? "green":"purple"}-400 shadow-sm">
                 <div class="cardInfo p-4 space-y-3">
                     <div class="statusBar flex justify-between items-center">
                         <div class="cardStatus w-full">
@@ -126,4 +177,5 @@ document.getElementById('searchField').addEventListener('input', event => {
     })
     })
 })
+
 
